291cc077157f66eaac60c2859c411822522675c3
kailashgautham <103820198+kailashgautham@users.noreply.github.com>
2026-02-14 00:18:48 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,4 +7,7 @@ int main()
   std::cerr << std::unitbuf;
 
    std::cout << "$ ";
+    std::string command;
+    std::getline(std::cin, command);
+    std:: cout << command << ": command not found" << std::endl;
 }
